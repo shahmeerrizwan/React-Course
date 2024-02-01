692070b915f05508cb02bab6bd6ff0e3182c22a5
shahmeer09 <shahmeerrizwan921@gmail.com>
2024-02-01 13:07:14 +0500

    Case-Converter-Project

--- 06-Text-Capitalize-Project/src/App.js
+++ 06-Text-Capitalize-Project/src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 
 function App() {
-  const [text, setText] = useState("Enter Text Here")
+  const [text, setText] = useState("")
   
 const upClick = ()=>{
   // console.log("clicked");
@@ -24,7 +24,7 @@ const onClick = (event)=>{
   
   return (
  
-     
+
 <>
 <div >
        <h1 className='text-center mt-5 mb-5'> Enter Any Text You Want To Uppercase Or Lowercase :</h1>
@@ -32,12 +32,23 @@ const onClick = (event)=>{
     
 <div className="mb-3 container">
  
-  <textarea className="form-control  " value={text} onChange={onClick} id="same" rows="6"></textarea>
+  <textarea className="form-control " name="comment" form="usrform" placeholder='Enter Any Text You Want To Uppercase Or Lowercase' value={text} onChange={onClick} id="same" rows="2" cols="50"></textarea>
 
 <button type="button" className="btn btn-success m-2 mt-4 "  onClick={upClick}>Convert To Upercase</button>
      <button type="button" className="btn btn-success m-2 mt-4" onClick={lowerCase}> Convert To Lower Case</button>
      </div>
 </div>
+
+<div className="container my-3">
+  <h1>Your Text Summary :</h1>
+  <p className='bold'> {text.split(" ").length} Words And {text.length} Character</p>
+  <p className='bold'>{0.008 * text.split(" ").length} Minutes Read</p>
+  <h1>Preview :</h1>
+  <textarea className="form-control " name="comment" form="usrform" placeholder='Preview' value={text} onChange={onClick} id="same" rows="12" cols="50"> {text} </textarea>
+
+</div>
+
+
 </>
      
   );

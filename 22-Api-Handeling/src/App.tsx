5c51034fed949ecde1b155b22d276c80a436153a
Muhammad Shahmeer Rizwan <148860820+shahmeerrizwan@users.noreply.github.com>
2024-06-02 02:11:30 +0500

    Api-Handeling

---
+++ 22-Api-Handeling/src/App.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+
+const [dataList, setDataList]=useState<any>([])
+
+const callApi = ()=>{
+  axios.get("https://jsonplaceholder.typicode.com/users")
+  .then((res)=>{
+    console.log(res.data);
+    setDataList([...res.data])
+   }) 
+   .catch((err)=>{
+      console.log(err);
+  })
+}
+
+const postData = () => {
+  axios.post('https://jsonplaceholder.typicode.com/posts', {
+      userId: 1,
+      title: "Something Important",
+      body: "ajsdk jas lkdjak la la lkdalk dlsa lkdsaj"
+  }).then(res => {
+      console.log(res)
+  }).catch((err) => {
+      console.log(err)
+  })
+}
+
+
+const editDat=()=>{
+  axios.put("https://jsonplaceholder.typicode.com/posts/3",{
+    userId:1,
+    tittle:"shahmeer",
+    body:"something"
+  }).then(res=>{
+console.log(res);
+}).catch((err)=>{
+  console.log(err);
+})
+}
+
+const deleteData = () => {
+  axios.delete('https://jsonplaceholder.typicode.com/posts/2')
+      .then(res => {
+          console.log(res)
+      }).catch((err) => {
+          console.log(err)
+      })
+}
+
+  return (
+    <>
+      <button onClick={callApi}>Get Api Call</button>
+      <button onClick={postData}>Post Api Call</button>
+      <button onClick={editDat}>Edit Api Call</button>
+      <button onClick={deleteData}>Delete Data</button>
+
+
+<table>
+<tbody>
+  <tr>
+    <th>ID </th>
+    <th>NAME </th>
+    <th>USERNAME </th>
+    <th>WEBSITE</th>
+  </tr>
+{
+dataList.map((x:any,i:number)=><tr  key={i}>
+  <td>{x.id}</td>
+  <td>{x.name}</td>
+  <td>{x.username}</td>
+  <td>{x.website}</td>
+</tr>
+)}
+</tbody>
+</table>
+</>
+  );
+}
+
+export default App;
